8848825be53f21d18068f7bfdae3f37263a909b9
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2023-02-17 11:10:31 -0300

    ''

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:app_flutter/app/core/hive.dart';
 import 'package:app_flutter/app/my_app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await HiveInit.load();
   runApp(const MyApp());
 }
